5bf7d670e55a9ac25229f9576967625290ccb573
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-12-15 11:10:16 -0500

    added base html using bootstrap. Included cdn links for JQUERY and Firebase

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCVA_-jmwN87lGZk-uFGxcudTGzQO5zuLg",
+    authDomain: "timecard-project.firebaseapp.com",
+    databaseURL: "https://timecard-project.firebaseio.com",
+    projectId: "timecard-project",
+    storageBucket: "",
+    messagingSenderId: "711958370286"
+  };
+  firebase.initializeApp(config);
+
+
+$(document).ready(function(){
+
+$("#submit").on("click", function(){
+
+
+});
+
+
+
+});
