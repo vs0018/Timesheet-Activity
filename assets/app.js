8edcfe3238c5b4b137770a62e6ef0420a99cd960
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-12-15 11:19:36 -0500

    Initialize Firebase DB and add button logic

--- assets/app.js
+++ assets/app.js
@@ -9,13 +9,24 @@
   };
   firebase.initializeApp(config);
 
-
+  var database = firebase.database();
+  
 $(document).ready(function(){
 
-$("#submit").on("click", function(){
+    $("#submit").on("click", function(){
+        var name = $("#empName").val().trim();
+        var role = $("#empRole").val().trim();
+        var start = $("#empStart").val().trim();
+        var billed = $("#totBilled").val().trim();
 
+        database.ref().push({
+            name: name,
+            role: role,
+            start: start,
+            billed: billed
+        });
 
-});
+    });
 
 
 
